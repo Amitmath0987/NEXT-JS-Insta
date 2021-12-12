c6ae0eb677646aabccfb363b4932efb406b3cd23
Amit Mathur <amitmathur349@gmail.com>
2021-12-12 18:29:16 +0530

    Add snackbars to show errors and success and handle that in globall api service to show.

--- pages/_app.js
+++ pages/_app.js
@@ -4,17 +4,31 @@ import { Provider } from "react-redux";
 import createSagaMiddleware from "redux-saga";
 import rootReducer from "redux/reducers";
 import rootSaga from "../redux/sagas/index";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function MyApp({ Component, pageProps }) {
   const sagaMiddleware = createSagaMiddleware();
-
-  const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+  const middleWares = [sagaMiddleware];
+  // const composeEnhancers =
+  //   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const store = createStore(rootReducer, {}, applyMiddleware(...middleWares));
 
   sagaMiddleware.run(rootSaga);
   return (
     <>
       <Provider store={store}>
         <Component {...pageProps} />
+        <ToastContainer
+          position="top-right"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
       </Provider>
     </>
   );
---
+++ utils/snackBar.js
@@ -0,0 +1,24 @@
+import { toast } from "react-toastify";
+export const errorSnackBar = (message) => {
+  toast.error(message || "error", {
+    position: "top-right",
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
+
+export const successSnackBar = (message) => {
+  toast.success(message || "success", {
+    position: "top-right",
+    autoClose: 2000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
